2bc5fd69e84182eccca7830e165ffaf94947795e
Murch <s.taldybaev@dats.team>
2023-09-12 13:11:56 +0600

    2

---
+++ src/test/java/api/FirstTest.java
@@ -0,0 +1,11 @@
+package api;
+
+import org.testng.annotations.Test;
+public class FirstTest {
+
+
+    @Test(description = "Создание KPI")
+    public void test() {
+        System.out.println("Hello, World!");
+    }
+}
